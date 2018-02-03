9c5bfb497c4bc99ff80a60a14e073899084a2207
Márvio André Bezerra Silverio <marvio.bezerra@hotmail.com>
2018-02-03 18:58:33 -0500

    Lab 02

---
+++ COMP305-W2018/Assets/Scripts/Lab02/Lab02.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lab02 : MonoBehaviour {
+
+    public SpriteRenderer image;
+
+    public Sprite sprite;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(buttonClick);
+    }
+
+    public void buttonClick () {
+        image.sprite = sprite;
+	}
+}
